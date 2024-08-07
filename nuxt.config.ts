3725ba5565792461dfcfa0d6bc3f4e9cbe35143c
guangbai <guangbai01@doulaoban.com>
2024-08-07 14:10:24 +0800

    feat: 部署

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const path = require('path')
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [ '@element-plus/nuxt', '@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, 'docs')
+    }
+  }
 })
